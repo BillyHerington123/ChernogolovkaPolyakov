2b822c19d016470f178acc8bc5ce49a7aaf31a24
Student_23 <d.polyakow2017@yandex.ru>
2021-12-07 16:12:48 +0400

    I'm Billy Herington123

---
+++ abc.py
@@ -0,0 +1,14 @@
+def my_superfunction():
+    print('Yandexlyceum')
+
+
+def main():
+    print('Yandexlyceum')
+    print('And I change it every day')
+    print('Again')
+    print('U')
+    my_superfunction()
+
+
+if __name__ == '__main__':
+    main()
